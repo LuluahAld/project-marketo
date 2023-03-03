2db0ae330ffe8a8104b7fa911d7c340c427e4b1d
Luluah <laldakhil0001@stu.kau.edu.sa>
2023-03-03 14:41:33 +0300

    adding constant file for color pallete

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+//color pallete
+
+import 'package:flutter/material.dart';
+
+var green = const Color.fromARGB(1, 63, 77, 57);
+var pink = const Color.fromARGB(1, 219, 183, 171);
+var beige = const Color.fromARGB(1, 241, 237, 236);
+var lightgreen = const Color.fromARGB(1, 146, 164, 139);
