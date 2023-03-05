4b1f77e4a7d475665830d4daf8477b09678113a4
Luluah <laldakhil0001@stu.kau.edu.sa>
2023-03-05 11:12:29 +0300

    added logo asset to consts

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,3 +6,5 @@ var green = const Color.fromARGB(1, 63, 77, 57);
 var pink = const Color.fromARGB(1, 219, 183, 171);
 var beige = const Color.fromARGB(1, 241, 237, 236);
 var lightgreen = const Color.fromARGB(1, 146, 164, 139);
+
+var logo = Image.asset('images/logo.png');
