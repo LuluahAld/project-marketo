0d28679e6de3a5c5c97ee90f2951ef231b5d55dc
Luluah <laldakhil0001@stu.kau.edu.sa>
2023-03-05 11:24:43 +0300

    added pages folder

--- lib/marceto_app.dart
+++ lib/marceto_app.dart
@@ -1,11 +1,13 @@
-
 import 'package:flutter/material.dart';
+import 'package:project_teamd/pages/onboarding_pages.dart';
 
 class MarcetoApp extends StatelessWidget {
   const MarcetoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: OnBoardingPages(),
+    );
   }
 }
