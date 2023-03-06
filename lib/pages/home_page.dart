51369cbcb1ebd969a3b06872735bd42e7b19b522
HanaDevFawa <112336255+HanaDevFawa@users.noreply.github.com>
2023-03-06 11:22:17 +0300

    Order Page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../constants/padding.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: padding,
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
