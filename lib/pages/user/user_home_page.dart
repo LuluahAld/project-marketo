6308baf6556d405f760086fbd5f5f35a3336cbe7
Luluah <laldakhil0001@stu.kau.edu.sa>
2023-03-06 13:39:30 +0300

    adjusting homepage

--- lib/pages/user/user_home_page.dart
+++ lib/pages/user/user_home_page.dart
@@ -14,124 +14,125 @@ class UserHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: ListView(
+      padding: EdgeInsets.zero,
+      physics: const NeverScrollableScrollPhysics(),
       children: [
         Container(
           width: MediaQuery.of(context).size.width,
-          height: 135,
+          height: 180,
           color: grey,
           child: Column(
             children: const [
+              SizedBox(height: 44),
               Logo(size: 200),
               SizedBox(
                 height: 50,
-                width: 300,
+                width: 400,
                 child: SearchBar(),
               ),
             ],
           ),
         ),
-        StreamBuilder<Object>(
-            stream: null,
-            builder: (context, snapshot) {
-              return Column(
+        ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(8, 12, 8, 12),
+          children: [
+            MainCategory(
+              'Popular Seller',
+              'See All',
+              green,
+              lightgreen,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
                 children: [
-                  MainCategory(
-                    'Popular Seller',
-                    'See All',
-                    green,
-                    lightgreen,
+                  ProductCard(
+                    image: 'images/single-person.png',
+                    text: 'Sameera',
                   ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        ProductCard(
-                          image: 'image/single-person.png',
-                          text: 'Sameera',
-                        ),
-                        ProductCard(
-                          image: 'image/single-person2.png',
-                          text: 'SM',
-                        ),
-                        ProductCard(
-                          image: 'image/single-person1.png',
-                          text: 'Noor',
-                        ),
-                      ],
-                    ),
+                  ProductCard(
+                    image: 'images/single-person2.png',
+                    text: 'SM',
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  MainCategory(
-                    'Recently Viewed',
-                    'See All',
-                    green,
-                    lightgreen,
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        ProductCard(
-                          image: 'image/pic1.png',
-                          text: 'T-Shirt',
-                        ),
-                        ProductCard(
-                          image: 'image/bag1.jpg',
-                          text: 'Bag',
-                        ),
-                        ProductCard(
-                          image: 'image/watch1.jpg',
-                          text: 'Watch',
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Container(
-                        padding: padding,
-                        decoration: BoxDecoration(
-                          color: lightgrey,
-                          borderRadius: borderRad,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            MText(
-                                text: 'Share & get Up to \$100 off',
-                                fontweight: FontWeight.bold,
-                                color: lightgreen,
-                                size: 16),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Give friends a 10% discount up to \$100 off their first Marketo order',
-                              style: TextStyle(color: lightgreen),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              children: [
-                                MText(text: 'Invite Friends', fontweight: FontWeight.bold, color: lightgreen, size: 16),
-                                Icon(
-                                  Icons.arrow_forward,
-                                  color: green,
-                                ),
-                              ],
-                            )
-                          ],
-                        )),
+                  ProductCard(
+                    image: 'images/single-person.png',
+                    text: 'Noor',
                   ),
                 ],
-              );
-            }),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            MainCategory(
+              'Recently Viewed',
+              'See All',
+              green,
+              lightgreen,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ProductCard(
+                    image: 'images/pic1.png',
+                    text: 'T-Shirt',
+                  ),
+                  ProductCard(
+                    image: 'images/bag1.jpg',
+                    text: 'Bag',
+                  ),
+                  ProductCard(
+                    image: 'images/watch1.jpg',
+                    text: 'Watch',
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                  padding: padding,
+                  decoration: BoxDecoration(
+                    color: lightgrey,
+                    borderRadius: borderRad,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      MText(
+                          text: 'Share & get Up to 100 SAR off',
+                          fontweight: FontWeight.bold,
+                          color: lightgreen,
+                          size: 16),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Give friends a 10% discount up to 100 SAR off their first Marketo order',
+                        style: TextStyle(color: lightgreen),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          MText(text: 'Invite Friends', fontweight: FontWeight.bold, color: lightgreen, size: 16),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: green,
+                          ),
+                        ],
+                      )
+                    ],
+                  )),
+            ),
+          ],
+        ),
       ],
     ));
   }
