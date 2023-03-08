fc0130cea5804fae911ad1aebf22982abcf76056
HanaDevFawa <112336255+HanaDevFawa@users.noreply.github.com>
2023-03-08 14:35:33 +0300

    Update user_home_page.dart

--- lib/pages/user/user_home_page.dart
+++ lib/pages/user/user_home_page.dart
@@ -10,9 +10,11 @@ import 'package:project_teamd/constants/color_pallete.dart';
 import 'package:project_teamd/constants/padding.dart';
 import 'package:project_teamd/model/appData.dart';
 import 'package:project_teamd/pages/user/cart.dart';
+import 'package:project_teamd/pages/user/user_product_details.dart';
 import 'package:project_teamd/pages/user/view_seller.dart';
 
-import '../../components/product/product_cart.dart';
+import '../../components/product/product_card.dart';
+import '../../components/seller_card_home.dart';
 import '../../model/product.dart';
 import '../../model/seller.dart';
 
@@ -51,7 +53,7 @@ class _UserHomePageState extends State<UserHomePage> {
   }
 
   Future<List<Seller>> getSeller() async {
-    final collection = await FirebaseFirestore.instance.collection('restaurant').get();
+    final collection = await FirebaseFirestore.instance.collection('seller').get();
     List<Seller> newList = [];
     for (final doc in collection.docs) {
       final seller = Seller.fromMap(doc.data());
@@ -106,124 +108,113 @@ class _UserHomePageState extends State<UserHomePage> {
       setState(() {});
     }
     return Scaffold(
-        body: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 180,
-              color: grey,
-              child: Column(
-                children: const [
-                  SizedBox(height: 56),
-                  Logo(size: 200),
-                  SizedBox(height: 8),
-                  SizedBox(
-                    height: 50,
-                    width: 400,
-                    child: SearchBar(),
-                  ),
-                ],
-              ),
+        body: Column(children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 180,
+            color: grey,
+            child: Column(
+              children: const [
+                SizedBox(height: 56),
+                Logo(size: 200),
+                SizedBox(height: 8),
+                SizedBox(
+                  height: 50,
+                  width: 400,
+                  child: SearchBar(),
+                ),
+              ],
             ),
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                padding: const EdgeInsets.all(12),
-                children: [
-                  MainCategory('Popular Seller', 'See All', green, lightgreen, 1),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => const ViewSeller()));
-                          },
-                          child: ProductCard(
-                            image: 'images/single-person.png',
-                            text: 'Sameeraa',
-                          ),
-                        ),
-                        ProductCard(
-                          image: 'images/single-person2.png',
-                          text: 'Hadi',
-                        ),
-                        ProductCard(
-                          image: 'images/single-person.png',
-                          text: 'Hana',
-                        ),
-                      ],
+          ),
+          Expanded(
+              child: ListView(shrinkWrap: true, padding: const EdgeInsets.all(12), children: [
+            MainCategory('Popular Seller', 'See All', green, lightgreen, 1),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(children: [
+                for (final sel in seller)
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const ViewSeller()));
+                    },
+                    child: SellerCardHome(
+                      seller: sel,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  MainCategory('Recently Viewed', 'See All', green, lightgreen, 2),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        ProductCard(
-                          image: 'images/pic1.png',
-                          text: 'T-Shirt',
+                  )
+              ]),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            MainCategory('Recently Viewed', 'See All', green, lightgreen, 2),
+            ListView(padding: EdgeInsets.zero, physics: const BouncingScrollPhysics(), shrinkWrap: true, children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    for (final product in product)
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => UserProductDetails(
+                                        pro: product,
+                                      )));
+                        },
+                        child: ProsuctCard2(
+                          product: product,
+                          cardWidth: 200,
+                          productImgWidth: 200,
                         ),
-                        ProductCard(
-                          image: 'images/bag1.png',
-                          text: 'Bag',
-                        ),
-                        ProductCard(
-                          image: 'images/watch1.jpg',
-                          text: 'Watch',
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Container(
-                        padding: padding,
-                        decoration: BoxDecoration(
-                          color: lightgrey,
-                          borderRadius: borderRad,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            MText(
-                                text: 'Share & get Up to \$100 off',
-                                fontweight: FontWeight.bold,
-                                color: lightgreen,
-                                size: 16),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Give friends a 10% discount up to \$100 off their first Marketo order',
-                              style: TextStyle(color: lightgreen),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              children: [
-                                MText(text: 'Invite Friends', fontweight: FontWeight.bold, color: lightgreen, size: 16),
-                                Icon(
-                                  Icons.arrow_forward,
-                                  color: green,
-                                ),
-                              ],
-                            )
-                          ],
-                        )),
-                  ),
-                ],
+                      )
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
+              const SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                    padding: padding,
+                    decoration: BoxDecoration(
+                      color: lightgrey,
+                      borderRadius: borderRad,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MText(
+                            text: 'Share & get Up to \$100 off',
+                            fontweight: FontWeight.bold,
+                            color: lightgreen,
+                            size: 16),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Give friends a 10% discount up to \$100 off their first Marketo order',
+                          style: TextStyle(color: lightgreen),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            MText(text: 'Invite Friends', fontweight: FontWeight.bold, color: lightgreen, size: 16),
+                            Icon(
+                              Icons.arrow_forward,
+                              color: green,
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+              ),
+            ])
+          ])),
+        ]),
         floatingActionButton: Visibility(
           visible: appData.cartVisible,
           child: FloatingActionButton(
